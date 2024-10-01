1a75277fad704235a59dc6d0b05df1fab84f3bbf
dudu4leal <eduardoleal.ferreir4@gmail.com>
2024-10-01 20:55:22 -0300

    adiciona operadores ternários

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -31,11 +31,11 @@ public class Operadores {
         //numero = numero * -1;
          //System.out.println(numero);
 
-        int numero = 5;
+        //int numero = 5;
 
         //numero = numero + 2;
         //numero = numero + 1;
-        numero = numero;
+        //numero = numero;
         //System.out.println(numero ++);
         //System.out.println(numero);
         //System.out.println(++ numero);
@@ -44,13 +44,20 @@ public class Operadores {
         //System.out.println(-- numero);
 
 
-        boolean variavel = true;
+        //boolean variavel = true;
         //System.out.println(!variavel);
         //System.out.println(variavel);
 
         //variavel = !variavel;
         //System.out.println(variavel);
 
+        int a,b;
+        a=5;
+        b=6;
+
+        String resultado = a==b ? "verdadeiro" : "falso"; 
+
+        System.out.println(resultado);
 
     }
 
