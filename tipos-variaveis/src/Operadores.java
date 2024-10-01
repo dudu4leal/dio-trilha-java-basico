a2db965f350949210122b62f2a6e70c711f00e4d
dudu4leal <eduardoleal.ferreir4@gmail.com>
2024-10-01 19:44:21 -0300

    adiciona resumo sobre operadores unários

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,26 +1,51 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        //String nomeCompleto = "LINGUAGEM" + "JAVA";
 
-        System.out.println(nomeCompleto);
+       // System.out.println(nomeCompleto);
 
-        String concatenacao = "?";
+        //String concatenacao = "?";
 
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
+        //concatenacao = 1+1+1+"1";
+        //System.out.println(concatenacao);
         
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
+        //concatenacao = 1+"1"+1+1;
+        //System.out.println(concatenacao);
 
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
+        //concatenacao = 1+"1"+1+"1";
+        //System.out.println(concatenacao);
 
-        concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
+        //concatenacao = "1"+1+1+1;
+        //System.out.println(concatenacao);
         
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+        //concatenacao = "1"+(1+1+1);
+        //System.out.println(concatenacao);
+
+        int numero = 5;
+
+        numero = - numero;
+        //System.out.println(- numero);
+        System.out.println(numero);
+
+        numero = numero * -1;
+         System.out.println(numero);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
