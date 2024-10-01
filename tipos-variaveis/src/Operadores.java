523265e48e36b1ddd8712f525a86fa7e61e37dc5
dudu4leal <eduardoleal.ferreir4@gmail.com>
2024-10-01 20:34:51 -0300

    adicionando operadores de incrementação e decrementação

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -22,28 +22,34 @@ public class Operadores {
         //concatenacao = "1"+(1+1+1);
         //System.out.println(concatenacao);
 
+        //int numero = 5;
+
+        //numero = - numero;
+        //System.out.println(- numero);
+        //System.out.println(numero);
+
+        //numero = numero * -1;
+         //System.out.println(numero);
+
         int numero = 5;
 
-        numero = - numero;
-        //System.out.println(- numero);
-        System.out.println(numero);
-
-        numero = numero * -1;
-         System.out.println(numero);
-
-
-
-
-
+        //numero = numero + 2;
+        //numero = numero + 1;
+        numero = numero;
+        //System.out.println(numero ++);
+        //System.out.println(numero);
+        //System.out.println(++ numero);
+        //System.out.println(numero --);
+        //System.out.println(numero);
+        //System.out.println(-- numero);
 
 
+        boolean variavel = true;
+        //System.out.println(!variavel);
+        //System.out.println(variavel);
 
-
-
-
-
-
-
+        //variavel = !variavel;
+        //System.out.println(variavel);
 
 
     }
